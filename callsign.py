e9f66133107451ae8170e87eaa634052bd70f04b
Robovanguard317 <43590623+Robovanguard317@users.noreply.github.com>
2018-09-29 11:59:00 -0500

    Add files via upload

--- callsign.py
+++ callsign.py
@@ -1,13 +1,14 @@
 import random
 from tkinter import *
 
-class CallsignGenertor () :
+class CallsignGenerator () :
     origFirst = ""
     origLast = ""
 
     firstList= ["Agile","Able","Proud","Just","Electric","Nimble","Swift","Silent","Saucy","Bold","Bold","Viscious","Icy"]
     lastList = ["Cobra","Badger","Bones","Dagger",]
 
+    #methods
     def __init__(self,firstDate, lastDate):
         self.origFirst = firstDate
         self.origLast = lastDate
@@ -16,11 +17,23 @@ class CallsignGenertor () :
         x= random.randint(0, len (self.firstList) -1)
         y=random.randint(0, len (self.lastList) -1)
         return self.firstList [x] + self.lastList [y]
-                
-#mycallsign = CallsignGenertor ("10","7")
-#print(mycallsign.origFirst, mycallsign.origLast)
-#print(mycallsign.CreateName() )
 
+
+def buttonClick():
+    #get the info out of the boxes
+    first = fText.get()
+    last = lText.get()
+    
+    #Create an instance of our generator class
+    mygen = CallsignGenerator(first, last)
+    
+    #run the generator
+    cSign = mygen.CreateName()
+                                    
+    #show it on the screen
+    output.config (text = cSign, image=Banner, compound = CENTER)
+
+                
 root = Tk()
 
 Banner = PhotoImage(file="USAF_Symbol.gif")
@@ -32,7 +45,7 @@ fLabel = Label(root, text="Month", font= ft)
 fText = Entry(root, font="Verdana 14")
 lLabel = Label(root, text="Day", font= ft)
 lText = Entry(root, font="Verdana 14")
-btn = Button(root, font = ft, text="generate name")
+btn = Button(root, font = ft, text="generate name", command=buttonClick)
 output = Label(root, image=Banner)
 
 title.pack()
